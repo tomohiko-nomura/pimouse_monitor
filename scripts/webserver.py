bec77ce0ec5675d39cf5cf2338c57d182c3e4b4f
Tomohiko Nomura <tomohiko.nomura@sound.ocn.ne.jp>
2018-11-02 09:37:23 +0900

    Change Script files.

--- scripts/webserver.py
+++ scripts/webserver.py
@@ -9,7 +9,3 @@ os.chdir(os.path.dirname(__file__) + "/../contents")
 rospy.init_node("webserver")
 rospy.on_shutdown(kill)
 SimpleHTTPServer.test()
-
-# Copyright 2016 Ryuichi Ueda
-# Released under the MIT License.
-# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
